ef1cd45d9f1c62201b489c19e823e35cf7f4bff2
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2017-08-04 15:50:12 -0300

    Added Socket.io and emit and recieve examples

--- routes/index.js
+++ routes/index.js
@@ -10,37 +10,7 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
 
-var token = '';
-request.get('https://stream.watsonplatform.net/authorization/api/v1/token?url=https://stream.watsonplatform.net/speech-to-text/api', {
-  'auth': {
-    'user': '08f3a6da-75df-4e84-88b5-8c6322b1054c',
-    'pass': 'YnTw5Bx826vm'
-  }
-},(error, response, body) => {
-  console.log(body);
-  token = body;
 
-  var wsURI = 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize?watson-token=' +
-  token + '&model=es-ES_BroadbandModel';
-  var websocket = new WebSocket(wsURI);
-  websocket.onopen = function(evt) { onOpen(evt) };
-  websocket.onclose = function(evt) { onClose(evt) };
-  websocket.onmessage = function(evt) { onMessage(evt) };
-  websocket.onerror = function(evt) { onError(evt) };
-
-  function onOpen(evt) {
-    var message = {
-      'action': 'start',
-      'content-type': 'audio/l16;rate=22050'
-    };
-    websocket.send(JSON.stringify(message));
-  }
-
-  function onMessage(evt) {
-    console.log(evt.data);
-  }
-
-});
 
 
 
